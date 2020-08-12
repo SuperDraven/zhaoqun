e20d78a27b3c8b9f1c4be81407cbd1c11980ebef
pc <pc@email.com>
2020-08-12 23:03:09 +0800

    '测试推送'

--- app/Http/Controllers/WechatController.php
+++ app/Http/Controllers/WechatController.php
@@ -8,11 +8,28 @@ use Illuminate\Http\Request;
 class WechatController extends Controller
 {
     //
+    public $wechatService;
+    public function __construct(WechatService $wechatServic)
+    {
+        $this->wechatService = $wechatServic;
+    }
 
-    public function Token(WechatService $wechatService)
+    public function Token()
     {
 
-        $response = $wechatService->app->server->serve();
+        $response = $this->wechatService->app->server->serve();
+        return $response;
+
+    }
+    public function getImg()
+    {
+        $string = "赵博";
+        $response = $this->wechatService->send_post("http://www.yishuzi.com/b/re13.php", ['id'=>$string]);
+        $this->wechatService->app->server->push(function ($message) use ($response) {
+//            return $response;
+            return "您好！欢迎来到 我要找群";
+        });
+        $response = $this->wechatService->app->server->serve();
         return $response;
 
     }
